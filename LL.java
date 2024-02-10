9eb0c1a9f297166a057e954ca7ea1ad9662024c6
meghana-raikar <raikarmeghana17@gmail.com>
2024-02-10 19:03:32 +0530

    dsa

--- LL.java
+++ LL.java
@@ -1,17 +1,13 @@
-package leetcode;
-/*
- * Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
-
- 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
-public class ListNode {
-      int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
- 
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         if(head == null) {
